851492f5cfec17ce8d71a0ea367ffd0a34d253f8
Lucas <lucas.reverdito@anima.edu.uy>
2024-12-10 11:18:23 -0300

    Primera resenia completa

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 const express = require('express');
 const app = express();
 
+let resenia = {
+  "Nombre_pelicula" : "Cars",
+  "Opinion" : "La pelicula es horrible, ademas, es muy poco realista, como un auto va a hablar?",
+  "Puntuacion" : "-10/10",
+  "Nombre_usuario" : "Lukita009"
+}
+
 app.get('/resenia', (_, res) => {
-  res.send("Cars");
-});
-
-app.get('/opinion', (_, res) => {
-  res.send("La pelicula es horrible, menos 10 sobre 10");
-});
-
-app.get('/puntuacion', (_, res) => {
-  res.send("-10/10");
-});
-
-app.get('/Me_gusta', (_, res) => {
-  res.send("No");
-});
-
-app.get('/No_me_gusta', (_, res) => {
-  res.send("Si");
-});
-
-app.get('/Nombre_usu', (_, res) => {
-  res.send("Lukita009");
-});
-
-app.get('/:nombre', (req, res) => {
-  // <- Aquí va la linea de código que debes escribir
-  res.send(`Tu nombre es ${_______}`);
+  res.send(resenia);
 });
 
 app.listen(3000);
