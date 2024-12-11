0e3417abd626ad0f9a318b1c1107a00ad729fa37
Lucas <lucas.reverdito@anima.edu.uy>
2024-12-11 09:46:25 -0300

    Mejora de la lista de resenias

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 
-let resenia = {
-  "Nombre_pelicula" : "Cars",
-  "Opinion" : "La pelicula es horrible, ademas, es muy poco realista, como un auto va a hablar?",
-  "Puntuacion" : "-10/10",
-  "Nombre_usuario" : "Lukita009"
-}
+let resenias = [
+  {Nombre_de_la_Pelicula: "Cars", Opinion : "La pelicula es horrible, ademas, es muy poco realista, como un auto va a hablar?", Puntuacion : "-10/10", Usuario : "lukita009"},
+  {Nombre_de_la_Pelicula: "Shrek", Opinion: "Y el realismo? Los Ogros no existen, ademas, los burros no hablan", Puntuacion : "-10/10 y bad", Usuario : "pepito83"},
+  {Nombre_de_la_Pelicula: "Mi pobre angelito", Opinion: "Es muy graciosa y entretenida", Puntuacion : "8/10", Usuario : "CristianoRonaldoSIUUU"}
+]
 
-app.get('/resenia', (_, res) => {
-  res.send(resenia);
+app.get('/resenias', (_, res) => {
+  res.send(resenias);
 });
 
 app.listen(3000);
